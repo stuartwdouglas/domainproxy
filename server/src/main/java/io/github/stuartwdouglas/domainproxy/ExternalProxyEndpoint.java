888b64274626a2f4f902bfc06cb0d0d8b8c52c4f
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-05-20 14:24:41 +1000

    More work

--- server/src/main/java/io/github/stuartwdouglas/domainproxy/ExternalProxyEndpoint.java
+++ server/src/main/java/io/github/stuartwdouglas/domainproxy/ExternalProxyEndpoint.java
@@ -1,11 +1,7 @@
 package io.github.stuartwdouglas.domainproxy;
 
-import io.netty.channel.epoll.Epoll;
 import io.quarkus.logging.Log;
 import io.vertx.core.Vertx;
-import io.vertx.core.impl.transports.EpollTransport;
-import io.vertx.core.impl.transports.KQueueTransport;
-import io.vertx.core.spi.transport.Transport;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
@@ -24,49 +20,33 @@ import java.util.Map;
 public class ExternalProxyEndpoint {
 
     final Client client;
-    final Map<String, List<String>> proxyTargets;
+    final Map<String, String> proxyTargets;
 
     public ExternalProxyEndpoint(Config config,
                                  @ConfigProperty(name = "proxy-paths") List<String> endpoints, Vertx vertx) {
         client = ClientBuilder.newBuilder()
                 .build();
-        Map<String, List<String>> targets = new HashMap<>();
+        Map<String, String> targets = new HashMap<>();
         for (var endpoint : endpoints) {
-            var proxyTarget = config.getConfigValue("proxy-path." + endpoint + ".targets").getValue().split(",");
-            targets.put(endpoint, List.of(proxyTarget));
+            var proxyTarget = config.getConfigValue("proxy-path." + endpoint + ".target").getValue();
+            targets.put(endpoint, proxyTarget);
         }
         this.proxyTargets = targets;
-        Transport transport = null;
-        try {
-            Transport epoll = new EpollTransport();
-            if (epoll.isAvailable()) {
-                System.out.println(epoll);
-            } else {
-                transport = epoll;
-            }
-        } catch (Throwable ignore) {
-            // Jar not here
-        }
-        System.out.println("NOT HERE");
     }
 
     @GET
     @Path("{root}/{path:.*}")
     public InputStream get(@PathParam("root") String root, @PathParam("path") String path) {
-        var targets = proxyTargets.get(root);
-        if (targets == null) {
+        var target = proxyTargets.get(root);
+        if (target == null) {
             throw new NotFoundException();
         }
-        for (var target : targets) {
-            var response = client.target(target + "/" + path).request().get();
-            if (response.getStatus() != 200) {
-                Log.errorf("Response %s %s", response.getStatus(), response.readEntity(String.class));
-                continue;
-            }
-            return response.readEntity(InputStream.class);
+        var response = client.target(target + "/" + path).request().get();
+        if (response.getStatus() != 200) {
+            Log.errorf("Response %s %s", response.getStatus(), response.readEntity(String.class));
+            throw new NotFoundException();
         }
-        throw new NotFoundException();
+
+        return response.readEntity(InputStream.class);
     }
-
-
 }
